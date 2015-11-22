44cb6e27f804f44ed1e30f5fc611e7d806031fc8
michele <michele.bortol@gmail.com>
2015-11-22 21:26:21 +0100

    added manager & crypt file

---
+++ crypto.py
@@ -0,0 +1,35 @@
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+import hashlib
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+iv= ':\x91N2e\xb0^)\xa3}\xc9\x02\xcf\x01\xae\x11'
+
+class AESCipher:
+    def __init__(self, key):
+        self.key = key
+
+    def encrypt(self, raw):
+        raw = pad(raw)
+        # iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        #iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return unpad(cipher.decrypt(enc[16:]))
+
+
+if __name__ == "__main__":
+    key_string = "xxxxxx"
+    cipher = AESCipher(hashlib.md5(key_string).hexdigest())
+
+    enc = cipher.encrypt('xoxp-xxxx')
+    print enc
+    print cipher.decrypt(enc)
--- manager.py
+++ manager.py
@@ -2,51 +2,102 @@ __author__ = 'michele'
 
 import requests
 import json
+from crypto import AESCipher
+import sys
+import hashlib
 
-slack_token = 'xoxp-14631819508-14631819700-15093267796-44ec2395f4'
+slack_token_ciphred = 'OpFOMmWwXimjfckCzwGuES/xpZpp9UAASuHlCaXFqAlf/Vd2Nwu1AzqUNSnvGdHht2Q15TVDEW12NdnORhsatzvSZFdvHUIFv3b5+gO/iPg='
+    # 'xoxp-14631819508-14631819700-15093267796-44ec2395f4'
 
-base_url ='https://slack.com/api/'
-
+base_url = 'https://slack.com/api/'
 
 method_auth_test = 'auth.test'
-method_channel_list='channels.list'
-method_groups_list='groups.list'
-method_im_list='im.list'
+method_channel_list = 'channels.list'
+method_groups_list = 'groups.list'
+method_im_list = 'im.list'
+method_chat_delete = 'chat.delete'
+
+slack_token = ''
 
 def _token():
-    return '?token='+slack_token
+    return '?token=' + slack_token
+
 
 def get_base_info():
-    response=requests.get(base_url+method_auth_test+'?token='+slack_token)
+    response = requests.get(base_url + method_auth_test + '?token=' + slack_token)
     return json.loads(response.content)
+
 
 def get_channel_list(exclude_archieved=False):
     if exclude_archieved:
-        exclude_archieved_param='&exclude_archieved=1'
+        exclude_archieved_param = '&exclude_archieved=1'
     else:
-        exclude_archieved_param='&exclude_archieved=0'
-    response = requests.get(base_url+method_channel_list+_token()+exclude_archieved_param)
+        exclude_archieved_param = '&exclude_archieved=0'
+    response = requests.get(base_url + method_channel_list + _token() + exclude_archieved_param)
 
     return json.loads(response.content).get('channels')
 
+
 def get_groups_list(exclude_archieved=False):
     if exclude_archieved:
-        exclude_archieved_param='&exclude_archieved=1'
+        exclude_archieved_param = '&exclude_archieved=1'
     else:
-        exclude_archieved_param='&exclude_archieved=0'
-    response = requests.get(base_url+method_groups_list+_token()+exclude_archieved_param)
+        exclude_archieved_param = '&exclude_archieved=0'
+    response = requests.get(base_url + method_groups_list + _token() + exclude_archieved_param)
 
     return json.loads(response.content).get('groups')
 
+
 def get_im_list():
-    response = requests.get(base_url+method_im_list+_token())
+    response = requests.get(base_url + method_im_list + _token())
     return json.loads(response.content).get('ims')
 
-if __name__ == "__main__":
-    base_info=get_base_info()
 
-    channels_list= get_channel_list()
+def delete_message(ts, channel):
+    ts_argument = '&ts=' + ts
+    channel_argument = '&channel=' + channel
+    response = requests.get(base_url + method_chat_delete + _token() + ts_argument + channel_argument)
+
+
+def _get_history(chat_type, channel, lastest=None, oldest=None, count=100, include_unreads=True):
+    channel_arg = '&channel=' + channel
+    lastest_arg = ('&lastest=' + lastest) if lastest else ''
+    oldest_arg = ('&oldtest=' + oldest) if oldest else ''
+    count_arg = '&count=' + str(count)
+    include_unreads_arg = '&unreads=1' if include_unreads else ''
+
+    response = requests.get(base_url + chat_type + '.history' + _token() + channel_arg + lastest_arg + oldest_arg +
+                            count_arg + include_unreads_arg)
+
+    response_dict = json.loads(response.content)
+    return response_dict.get('messages'), response_dict.get('has_more')
+
+
+def delete_all_channel_messages(channel_type, channel):
+    delete_all = lambda x: delete_message(x.get('ts'), channel)
+
+    messages, has_more = _get_history(channel_type, channel)
+
+    map(delete_all, messages)
+    while has_more:
+        messages, has_more = _get_history(channel_type, channel)
+        map(delete_all, messages)
+
+
+if __name__ == "__main__":
+
+    key = hashlib.md5(sys.argv[1]).hexdigest()
+
+    slack_token = AESCipher(key).decrypt(slack_token_ciphred)
+
+    base_info = get_base_info()
+
+    channels_list = get_channel_list()
     groups_list = get_groups_list()
     ims_list = get_im_list()
+
+    first_channel_id = channels_list[0].get('id')
+
+    history, has_more = _get_history('channels', first_channel_id, count=10)
 
     print base_info
